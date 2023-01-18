ea6c18210ba137ae28e7ea828470413024fb5c06
NaJunhyeok <njs4922@gmail.com>
2023-01-18 22:37:37 +0900

    1/18

--- 11720.cpp
+++ 11720.cpp
@@ -4,15 +4,16 @@ using namespace std;
 
 int main(){
     int n;
-    char arr[n];
+    char arr[100];
     int sum = 0;
     cin >> n;
 
-    cin >> arr;
+
 
     for(int i = 0; i < n; i++)
     {
-        sum += arr[i]-'0';
+        cin >> arr[i];
+        sum += arr[i]-'0'; //char형 이기 때문에 문자열 0을 마이너스ㅡ하여 아스키 코드값이 실제 숫자 값에 대응할 수 있도록 한다.
     }
     cout << sum << endl;
 }
