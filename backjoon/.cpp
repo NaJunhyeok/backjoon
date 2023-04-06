4dd913f4f3783b608ae768b5a3cb7da785a13e79
NaJunhyeok <njs4922@gmail.com>
2023-04-06 18:28:52 +0900

    test

---
+++ backjoon/.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a,b,c,d,e;
+    int total = 0;
+
+    cin >> a >> b >> c >> d >> e;
+
+    total = (a + b + c + d + e) / 5;
+    
+
+}
